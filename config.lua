f8889938139f5e02e8949f1a24bf23e94cacf00b
Agimir <william@mesaindigostudios.com>
2023-01-23 12:06:50 -0600

    Added JobGroups & JobItem1
    editing config for using groups and items in job

--- config.lua
+++ config.lua
@@ -1,1 +1,9 @@
+-- Apparently this is needed and must be empty
+Config = {}
 
+-- Groups that will be able to work / access all utilities of this job
+Config.JobGroups = {'job_name_1', 'job_name_2'}
+
+-- List of items that are required for doing X task (i.e need newspaper_stack to fill newspaper bin)
+Config.JobItem1 = {'item_name_1', 'job_item_2'}
+Config.JobItem2 = {'item_name_1', 'job_item_2'}
